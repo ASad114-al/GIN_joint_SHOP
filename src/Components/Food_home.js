574c6cf56744badcfc2a9729454eca35a30856ed
Asad114-al <as3ad.94aa@gmail.com>
2020-11-28 00:02:32 +0100

    css style2

--- src/Components/Food_home.js
+++ src/Components/Food_home.js
@@ -5,7 +5,7 @@ const Food = () => {
             <p className="uppercase">
             Cocktail & Food Menus
         </p>
-            <h2>Hand  Crafted   Cocktails & Damn Good Eats</h2>
+            <h2 id='hand'>Hand  Crafted <br/>  Cocktails & Damn Good Eats</h2>
             <p>We mix our gin and other spirits with <br/> the finest and freshest fruits, juices, <br/> syrups and infusions in cocktails <br/> inspired by original recipes.</p>
             <button id='btn'> See Menu</button>
         </div>
